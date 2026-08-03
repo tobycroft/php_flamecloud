0d528b7fb0fc4edfc6ea87bfab57b19f27a20148
tobycroft <24893075+tobycroft@users.noreply.github.com>
2026-08-03 10:05:07 +0800

    add CommonController and LoginedController

---
+++ app/controller/LoginedController.php
@@ -0,0 +1,51 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\controller;
+
+use think\App;
+use think\facade\Session;
+use think\Response;
+
+class LoginedController extends CommonController
+{
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $this->checkLogin();
+    }
+
+    protected function checkLogin(): void
+    {
+        $adminId = Session::get('admin_id');
+        if (!$adminId) {
+            $this->unauthorized();
+        }
+    }
+
+    protected function unauthorized(): void
+    {
+        if ($this->request->isAjax()) {
+            json(['code' => 401, 'msg' => '未登录或登录已过期'])->send();
+        } else {
+            redirect((string) url('auth/login'))->send();
+        }
+        exit;
+    }
+
+    protected function getAdminId(): int
+    {
+        return (int) Session::get('admin_id', 0);
+    }
+
+    protected function getAdminName(): string
+    {
+        return (string) Session::get('admin_name', '');
+    }
+
+    protected function getAdminUsername(): string
+    {
+        return (string) Session::get('admin_username', '');
+    }
+}
+EOF 
